d55dc2d65fb078513eecaf64d61892b5c5b2a87a
Daniel Bell <djrwb131@gmail.com>
2020-02-14 11:54:00 -0400

    Basic UI and (incomplete) import from JSON

--- quick_configurator.py
+++ quick_configurator.py
@@ -1,17 +1,85 @@
+#!/usr/bin/python3
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-class MainWindow(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self,title="Quick Configurator")
+try:
+    cfg = open("cmd_tree.json","r")
+    from json_db import Decoder
+    import json
+    ctree = json.load(cfg,cls=Decoder)
+    cfg.close()
+    
+except Exception as e:
+    print("ERROR LOADING JSON FILE: %s" % (e,))
+    import config_parser
+    config_parser.init()
+    ctree = config_parser.cmd_tree
 
-        self.box_commandline = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(self.box_commandline)
-        self.commandline = Gtk.Entry()
-        self.commandline.set_text("TEST")
-        self.box_commandline.pack_start(self.commandline,True,True,0)
-window = MainWindow()
-window.show_all()
-window.connect("destroy",Gtk.main_quit)
-Gtk.main()
+def hello(button):
+    print("Hello world!")
+    
+def goodbye(button):
+    print("Goodbye world!")
+    Gtk.main_quit()
+    
+def switchMode(commandModeTree):
+    global clist
+    
+    pass
+handlers = {
+    "onMainWindowDestroy": goodbye,
+    "hello": hello,
+    "commandModeChanged": switchMode
+    }
+builder = Gtk.Builder()
+builder.add_from_file("ui.glade")
+builder.connect_signals(handlers)
+    
+window = builder.get_object("mainWindow")
+gtree = builder.get_object("commandModeTreeStore")
+clist = builder.get_object("commandListStore")
+select = builder.get_object("commandModeSelection")
+modepass=list(ctree.get_all_modes())
+
+
+#This is ridiculous. We'll need to add gtk info into the config_parser itself
+#Getting rows by data within them is unusually hard - am I missing something?
+def get_iter_by_value(tree,col,value,row=False):
+    if not row:
+        row = tree.get_iter_first()
+    if tree[row][col] == value:
+        return row
+    if tree.iter_n_children(row) == 0:
+        return None
+    for i in range(tree.iter_n_children(row)):
+        ret = get_iter_by_value(tree,col,value,tree.iter_nth_child(row,i))
+        if ret:
+            return ret
+    return None
+
+while len(modepass) > 0:
+    removed = []
+    for i in modepass:
+        parent=ctree.get_parent_mode(i)
+        if parent == None:
+            r = gtree.append(None)
+            gtree.set(r, {0:i} )
+            removed.append(i)
+        else:
+            a = get_iter_by_value(gtree,0,str(parent))
+            if a:
+                r = gtree.append(a)
+                gtree.set(r,{0:i})
+                removed.append(i)
+    if len(removed) == 0:
+        print("Some modes could not be added, as their parents could not be found:")
+        for i in modepass:
+            print(i)
+        break
+    for i in removed:
+        modepass.remove(i)
+                       
+#window.show_all()  
+#Gtk.main()
+ 
