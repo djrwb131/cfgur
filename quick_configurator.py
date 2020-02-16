520b9d03ee0264e64563d7d7d23ea33119c19d68
Daniel Bell <djrwb131@gmail.com>
2020-02-15 21:51:52 -0400

    Command listing now working

--- quick_configurator.py
+++ quick_configurator.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 import gi
 
+#TODO: add Gtk support in subclasses of Command*
+# or find another clean way of implementing UI connections
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 import json_db
 
-try:
-    ctree = json_db.get_command_tree()
-except Exception as e:
-    print("ERROR LOADING JSON FILE: %s" % (str(e)))
-    exit(1)
-##    try:
-##        import config_parser
-##        config_parser.init()
-##        ctree = config_parser.cmd_tree
-##    except Exception as e:
-##        print("ERROR PARSING: %s" % (str(e)))
-##
+def load_json():
+    return json_db.get_command_tree()
+
+def reparse():
+    import config_parser
+    config_parser.init()
+    return config_parser.cmd_tree
+
 def hello(button):
     print("Hello world!")
     
@@ -24,27 +23,43 @@ def goodbye(button):
     print("Goodbye world!")
     Gtk.main_quit()
     
-def switchMode(commandModeTree):
-    global clist
-    
+def setSelectedCommandMode(commandModeTree):
+    global selected_mode
+    store, paths = commandModeTree.get_selection().get_selected_rows()
+    path = paths[0] # can't have mode than one mode active at a time
+    selected_mode = store[path][0]
+    print("Selected mode is now [ %s ]" % (str(selected_mode)))
     pass
+
+def updateCommandList(commandList):
+    global ctree
+    global selected_mode
+    if not selected_mode:
+        return
+    commandList.clear()
+    print("Found %i commands" % ( len(ctree.modes[selected_mode].commands.keys())))
+    for cmd in ctree.modes[selected_mode].commands.keys():
+        r = commandList.append()
+        commandList.set(r, { 0:cmd } )
+        
 handlers = {
     "onMainWindowDestroy": goodbye,
     "hello": hello,
-    "commandModeChanged": switchMode
+    "commandModeChanged": setSelectedCommandMode,
+    "commandListNeedsUpdate": updateCommandList
     }
 builder = Gtk.Builder()
 builder.add_from_file("ui.glade")
 builder.connect_signals(handlers)
-    
+
+ctree = reparse() 
 window = builder.get_object("mainWindow")
 gtree = builder.get_object("commandModeTreeStore")
 clist = builder.get_object("commandListStore")
-select = builder.get_object("commandModeSelection")
+selected_mode = None
 modepass=list(ctree.get_all_modes())
 
 
-#This is ridiculous. We'll need to add gtk info into the config_parser itself
 #Getting rows by data within them is unusually hard - am I missing something?
 def get_iter_by_value(tree,col,value,row=False):
     if not row:
@@ -59,6 +74,8 @@ def get_iter_by_value(tree,col,value,row=False):
             return ret
     return None
 
+# solves the issue of a command mode's parent not being loaded
+# does several passes; if the list gets smaller, do another pass
 while len(modepass) > 0:
     removed = []
     for i in modepass:
@@ -81,6 +98,6 @@ while len(modepass) > 0:
     for i in removed:
         modepass.remove(i)
                        
-#window.show_all()  
-#Gtk.main()
+window.show_all()  
+Gtk.main()
  
